20542471e722c45b1ae849ac06673a79cdd658d9
Johan van der Heide <info@jield.nl>
2022-02-08 12:36:17 +0100

    fixed the file upload
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- src/Form/View/Helper/FormFile.php
+++ src/Form/View/Helper/FormFile.php
@@ -5,14 +5,16 @@ namespace LaminasBootstrap5\Form\View\Helper;
 use Laminas\Form\ElementInterface;
 use Laminas\Form\View\Helper;
 
+use function count;
+
 final class FormFile extends Helper\FormFile
 {
     public function render(ElementInterface $element): string
     {
-        $element->setAttribute('class', 'form-control-file');
+        $element->setAttribute('class', 'form-control');
 
-        if (\count($element->getMessages()) > 0) {
-            $element->setAttribute('class', 'form-control-file is-invalid');
+        if (count($element->getMessages()) > 0) {
+            $element->setAttribute('class', 'form-control is-invalid');
         }
 
         $element->setAttribute('id', $element->getName());
