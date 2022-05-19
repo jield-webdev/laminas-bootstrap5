bcf7a43edb5d1e9e2ca0aa8b815690446744ab4d
Johan van der Heide <info@jield.nl>
2022-05-19 13:07:30 +0200

    Add support for slider
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- config/module.config.php
+++ config/module.config.php
@@ -6,50 +6,51 @@ use LaminasBootstrap5\Navigation;
 use LaminasBootstrap5\View\Helper;
 
 return [
-    'view_helpers'               => [
-        'aliases'    => [
+    'view_helpers' => [
+        'aliases' => [
 
             //Add some BC changes with previous module
-            'ztbalert'         => 'lbs5alert',
-            'ztbformelement'   => 'lbs5formelement',
+            'ztbalert' => 'lbs5alert',
+            'ztbformelement' => 'lbs5formelement',
             'filterbarelement' => 'lbs5filterbarelement',
 
-            'lbs5navigation'          => Helper\Navigation::class,
-            'lbs5filterbarelement'    => View\Helper\FilterBarElement::class,
+            'lbs5navigation' => Helper\Navigation::class,
+            'lbs5filterbarelement' => View\Helper\FilterBarElement::class,
             'lbs5filtercolumnelement' => View\Helper\FilterColumnElement::class,
-            'lbs5formelement'         => View\Helper\FormElement::class,
-            'lbs5formcheckbox'        => View\Helper\FormCheckbox::class,
+            'lbs5formelement' => View\Helper\FormElement::class,
+            'lbs5formcheckbox' => View\Helper\FormCheckbox::class,
 
         ],
-        'factories'  => [
-            Helper\Navigation::class               => Navigation\View\NavigationHelperFactory::class,
-            View\Helper\FormElement::class         => ConfigAbstractFactory::class,
-            View\Helper\FormCheckbox::class        => ConfigAbstractFactory::class,
-            View\Helper\FilterBarElement::class    => ConfigAbstractFactory::class,
+        'factories' => [
+            Helper\Navigation::class => Navigation\View\NavigationHelperFactory::class,
+            View\Helper\FormElement::class => ConfigAbstractFactory::class,
+            View\Helper\FormCheckbox::class => ConfigAbstractFactory::class,
+            View\Helper\FilterBarElement::class => ConfigAbstractFactory::class,
             View\Helper\FilterColumnElement::class => ConfigAbstractFactory::class,
         ],
         'invokables' => [
-            'lbs5formdescription'   => View\Helper\FormDescription::class,
+            'lbs5formdescription' => View\Helper\FormDescription::class,
             'lbs5formdatetimelocal' => View\Helper\FormDateTimeLocal::class,
-            'lbs5forminput'         => View\Helper\FormInput::class,
-            'lbs5formfile'          => View\Helper\FormFile::class,
-            'lbs5formradio'         => View\Helper\FormRadio::class,
-            'lbs5formtextarea'      => View\Helper\FormTextarea::class,
-            'lbs5formselect'        => View\Helper\FormSelect::class,
+            'lbs5forminput' => View\Helper\FormInput::class,
+            'lbs5formmultislider' => View\Helper\FormMultiSlider::class,
+            'lbs5formfile' => View\Helper\FormFile::class,
+            'lbs5formradio' => View\Helper\FormRadio::class,
+            'lbs5formtextarea' => View\Helper\FormTextarea::class,
+            'lbs5formselect' => View\Helper\FormSelect::class,
             'lbs5formmulticheckbox' => View\Helper\FormMultiCheckbox::class,
-            'lbs5alert'             => Helper\Alert::class,
+            'lbs5alert' => Helper\Alert::class,
         ],
     ],
     ConfigAbstractFactory::class => [
-        View\Helper\FormElement::class         => [
+        View\Helper\FormElement::class => [
             'ViewHelperManager',
             \Laminas\I18n\Translator\TranslatorInterface::class
         ],
-        View\Helper\FormCheckbox::class        => [
+        View\Helper\FormCheckbox::class => [
             'ViewHelperManager',
             \Laminas\I18n\Translator\TranslatorInterface::class
         ],
-        View\Helper\FilterBarElement::class    => [
+        View\Helper\FilterBarElement::class => [
             'ViewHelperManager',
             \Laminas\I18n\Translator\TranslatorInterface::class
         ],
---
+++ src/Form/View/Helper/FormMultiSlider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LaminasBootstrap5\Form\View\Helper;
+
+use Laminas\Form\Element\Range;
+use Laminas\Form\ElementInterface;
+use Laminas\Form\View\Helper;
+
+use function count;
+
+/**
+ *
+ */
+final class FormMultiSlider extends Helper\FormText
+{
+
+    public function render(ElementInterface $element): string
+    {
+        $id = md5($element->getName());
+
+        if (count($element->getMessages()) > 0) {
+            $element->setAttribute('class', $element->getAttribute('class') . ' is-invalid');
+        }
+
+        $element->setAttribute('id', $id);
+
+        //Grab the raw values (keys)
+        $rawValueOptions = array_keys($element->getValueOptions());
+
+        //Sort values from low to high
+        sort($rawValueOptions);
+
+        $ticks = implode(', ', $rawValueOptions);
+        $value = $element->getValue(); //The SearchFormResult helper transforms the result into an array
+
+        if (null === $value) {
+            $value = $ticks;
+        }
+
+        $element->setAttribute('data-provide', 'slider');
+        $element->setAttribute('data-slider-ticks', "[" . $ticks . "]");
+        $element->setAttribute('data-slider-ticks-labels', "[" . $ticks . "]");
+        $element->setAttribute('data-slider-value', "[" . $value . "]");
+        $element->setAttribute('data-slider-lock-to-ticks', true);
+
+        return parent::render($element);
+    }
+}
